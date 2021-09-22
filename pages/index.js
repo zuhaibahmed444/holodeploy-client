72d5064c72221024d54b8c9d75d15152c1dddf01
zuhaibahmed444 <zuhaibahmed444@gmail.com>
2021-09-23 05:16:24 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import { useState,useEffect} from "react";
+import axios from 'axios';
+import CourseCard from "../components/cards/CourseCard";
+
+const Index = ({courses}) => {
+  
+
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">
+        Online Education Marketplace
+        </h1>
+        <div className ="container-fluid">
+          <div className ="row">
+            {courses.map((course)=> <div key ={course._id}
+            className = "col-md-4">{
+              <CourseCard course = {course} />
+      
+            }
+
+               </div> )}
+          </div>
+
+        </div>
+       
+    </>
+  );
+};
+
+
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(`${process.env.API}/courses`);
+  // console.log("DATA LENGTH =====> ", data.length);
+  return {
+    props: {
+      courses: data,
+    },
+  };
+}
+
+export default Index;
